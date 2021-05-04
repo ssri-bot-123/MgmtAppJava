c6630d7fd58401f415f53cc6ac305f40ee38317d
sri <gamerbcoziam@gmail.com>
2021-05-04 20:23:10 +0530

    Added User Class which is the prototype for Users details to be stored in db

---
+++ src/main/java/com/example/MgmtApp/User.java
@@ -0,0 +1,75 @@
+package com.example.MgmtApp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true, length = 20)
+	private String email;
+	
+	@Column(nullable = false, length = 64)
+	private String password;
+	
+	@Column(nullable = false, length = 20)
+	private String firstName;
+	
+	@Column(nullable = false, length = 20)
+	private String lastName;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
+
+
+
+//@Entity
+//class to map into table inside db
+//by default same name as class name
+//even tho mysql not case sensitive it affects in some other areas
+//@Entity(name="<give ur own name>")
+
+//@Table by def table name same as entity name but can be customized
+//entity name used in jpql or hql
+
+// in sql table name is used
