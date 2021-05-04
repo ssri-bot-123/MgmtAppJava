a64907b0d9128fda959e1c457e8815d7ed719b36
sri <gamerbcoziam@gmail.com>
2021-05-04 18:32:46 +0530

    Original initialized files with maven dependencies

---
+++ src/main/java/com/example/MgmtApp/MgmtAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MgmtApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgmtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgmtAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MgmtApp/MgmtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MgmtApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgmtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
