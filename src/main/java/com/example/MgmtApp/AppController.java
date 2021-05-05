ef1cbd23d4f0638958fc3c0b477213d8aaa2ebe0
sri <gamerbcoziam@gmail.com>
2021-05-05 10:15:37 +0530

    Bootstrap used for styling - User Registration link provided - signup_form.html file added

--- src/main/java/com/example/MgmtApp/AppController.java
+++ src/main/java/com/example/MgmtApp/AppController.java
@@ -1,6 +1,7 @@
 package com.example.MgmtApp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,6 +10,12 @@ public class AppController {
 	@GetMapping("")
 	public String viewHomePage() {
 		return "index";
+	}
+	
+	@GetMapping("/register") 
+	public String showSignUpForm(Model model) {
+		model.addAttribute("user", new User());
+		return "signup_form";
 	}
 
 }
