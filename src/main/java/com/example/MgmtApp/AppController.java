2d33929ac80c7346a393a94b7dd842c545a41306
sri <gamerbcoziam@gmail.com>
2021-05-05 11:41:30 +0530

    Reg Successful. Values entered stored in DB true. Added Reg_success page. Check signup_form before running for possible errors in future

--- src/main/java/com/example/MgmtApp/AppController.java
+++ src/main/java/com/example/MgmtApp/AppController.java
@@ -1,11 +1,15 @@
 package com.example.MgmtApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
+	@Autowired
+	private UserRepository repo;
 	
 	@GetMapping("")
 	public String viewHomePage() {
@@ -16,6 +20,13 @@ public class AppController {
 	public String showSignUpForm(Model model) {
 		model.addAttribute("user", new User());
 		return "signup_form";
+	}
+	
+	@PostMapping("/process_register")
+	public String processRegistration(User user) {
+		repo.save(user);
+		
+		return "register_success";
 	}
 
 }
